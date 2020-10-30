c725aab7de2c3f782f8273ae4cc96faf21747290
Juraci Paixão Kröhling <juraci@kroehling.de>
2020-10-30 15:56:57 +0100

    Optimize binaries (#21)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- internal/builder/main.go
+++ internal/builder/main.go
@@ -95,7 +95,7 @@ func Compile(cfg Config) error {
 	}
 
 	cfg.Logger.Info("Compiling")
-	cmd := exec.Command(goBinary, "build", "-trimpath", "-o", cfg.Distribution.ExeName)
+	cmd := exec.Command(goBinary, "build", "-ldflags=-s -w", "-trimpath", "-o", cfg.Distribution.ExeName)
 	cmd.Dir = cfg.Distribution.OutputPath
 	if out, err := cmd.CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to compile the OpenTelemetry Collector distribution: %w. Output: %q", err, out)
